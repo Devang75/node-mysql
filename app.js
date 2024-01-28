2158c074026c90e6d3014d4ab75693d1b2aad0d6
Devang Patil <109170739+Devang75@users.noreply.github.com>
2024-01-28 18:33:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', async (req, res, next) => {
